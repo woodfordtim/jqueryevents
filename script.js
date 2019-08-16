150e524c303c3657ac3a74105931cef4d79e8425
Ubuntu <ubuntu@ip-172-31-92-18.ec2.internal>
2019-08-16 14:39:43 +0000

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
+    $("#button_effects1").click(function(){
+            $('#button_effects1').hide('slow');
 
-	$("#button_effects1").click(function(){
-		$("#button_effects1").hide("medium");
-	}
+        });
 }); 
