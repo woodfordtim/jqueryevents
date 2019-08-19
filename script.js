68fb0fa2a25468f9b610a29b5fb3e8650b8816f8
Ubuntu <ubuntu@ip-172-31-92-18.ec2.internal>
2019-08-19 11:35:27 +0000

    using toggle

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
-    $("#button_effects1").click(function(){
-            $('#button_effects1').hide('slow');
-
-        });
+	
+	$("#button_effects1").click(function(){
+        $('#par1').slideToggle('1000');
+    });
+    $("#button_effects2").click(function(){
+        $('#par2').slideToggle('1000');
+    });
+    $("#button_effects3").click(function(){
+        $('#par3').toggle('1000');
+    });
+    $("#button_effects4").click(function(){
+        $('#par4').toggle('1000');
+    });
+    $("#button_effects5").click(function(){
+        $('#par5').toggle('1000');
+    });
+    $("#button_effects6").click(function(){
+        $('#par6').toggle('1000');
+    });
+    
 }); 
